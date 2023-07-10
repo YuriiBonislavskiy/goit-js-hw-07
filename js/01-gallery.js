8f210ad44a437be492f9caa59d67e3f8b4775ca2
YuriiBonislavskiy <130743571+YuriiBonislavskiy@users.noreply.github.com>
2023-07-10 16:40:49 +0300

    Домашнє завдання 7

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+// import * as basicLightbox from 'basicLightbox';
+
+console.log(galleryItems);
+
+let instanceOfBasicLightbox;
+
+const galleryItemsRetdersString = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}" onclick="return false;">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+  })
+  .join("");
+
+const galleryList = document.querySelector(".gallery");
+galleryList.addEventListener("click", galleryClick);
+galleryList.insertAdjacentHTML("beforeend", galleryItemsRetdersString);
+
+function galleryClick(evt) {
+  const clickEl = evt.target;
+  const isGalleryItemClick = clickEl.classList.contains("gallery__image");
+
+  if (isGalleryItemClick) {
+    clickEl.classList.add();
+    const largeImage = evt.target.dataset.source;
+    const instanceString = `<img src="${largeImage}">`;
+    instanceOfBasicLightbox = basicLightbox.create(instanceString);
+    instanceOfBasicLightbox.show();
+    window.addEventListener("keydown", onEscapePress);
+  }
+}
+
+function onEscapePress(evt) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = evt.code === ESC_KEY_CODE;
+  if (isEscKey) {
+    instanceOfBasicLightbox.close();
+    window.removeEventListener("keydown", onEscapePress);
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+
+const galleryItemsRetdersString = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}"  onclick="return false;">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+  })
+  .join("");
+
+const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML("beforeend", galleryItemsRetdersString);
+
+const gallery = new SimpleLightbox('.gallery__item .gallery__link', {captionsData: 'alt', captionDelay: 250,});
